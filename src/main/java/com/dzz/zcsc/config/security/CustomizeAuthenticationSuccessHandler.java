f0d8ef12371055e1e29ea4539f95ede1c1ae8893
zori0 <776021342@qq.com>
2019-04-29 22:21:49 +0800

    init

--- src/main/java/com/dzz/zcsc/config/security/CustomizeAuthenticationSuccessHandler.java
+++ src/main/java/com/dzz/zcsc/config/security/CustomizeAuthenticationSuccessHandler.java
@@ -4,8 +4,11 @@ import com.alibaba.fastjson.JSON;
 import com.dzz.zcsc.common.response.ResponseDzz;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -17,13 +20,24 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
  */
 public class CustomizeAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
+    private TokenUtils tokenUtils;
+
+    public CustomizeAuthenticationSuccessHandler() {
+        try {
+            this.tokenUtils = new TokenUtils();
+        }catch(UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
             Authentication authentication) throws IOException {
-        response.setContentType("application/json;charset=utf-8");
-        PrintWriter out = response.getWriter();
 //        String message = "登录成功";
-        out.write(JSON.toJSONString(ResponseDzz.ok()));
+        response.setStatus(HttpStatus.OK.value());
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        PrintWriter out = response.getWriter();
+        out.write(JSON.toJSONString(ResponseDzz.ok(tokenUtils.generateToken(authentication))));
         out.flush();
         out.close();
     }
